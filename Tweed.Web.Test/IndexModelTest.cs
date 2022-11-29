bcc2d76abb5272cf967d8179abb8236ccf293922
Stephan Oehlert <stephan.oehlert@gmail.com>
2022-11-29 10:23:48 +0100

    Display UserName instead of User ID

--- Tweed.Web.Test/IndexModelTest.cs
+++ Tweed.Web.Test/IndexModelTest.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 using Tweed.Data;
+using Tweed.Data.Entities;
 using Tweed.Web.Pages;
 using Xunit;
 
@@ -8,11 +10,18 @@ namespace Tweed.Web.Test;
 
 public class IndexModelTest
 {
+    private readonly Mock<UserManager<AppUser>> _userManagerMock;
+
+    public IndexModelTest()
+    {
+        _userManagerMock = UserManagerMockHelper.MockUserManager<AppUser>();
+    }
+
     [Fact]
     public async Task OnGet_ShouldLoadTweeds()
     {
         var tweedQueriesMock = new Mock<ITweedQueries>();
-        var indexModel = new IndexModel(tweedQueriesMock.Object);
+        var indexModel = new IndexModel(tweedQueriesMock.Object, _userManagerMock.Object);
         await indexModel.OnGetAsync();
         tweedQueriesMock.Verify(t => t.GetLatestTweeds());
     }
--- Tweed.Web/Pages/Index.cshtml.cs
+++ Tweed.Web/Pages/Index.cshtml.cs
@@ -1,22 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NodaTime;
 using Tweed.Data;
+using Tweed.Data.Entities;
 
 namespace Tweed.Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ITweedQueries _tweedQueries;
+    private readonly UserManager<AppUser> _userManager;
 
-    public IndexModel(ITweedQueries tweedQueries)
+    public IndexModel(ITweedQueries tweedQueries, UserManager<AppUser> userManager)
     {
         _tweedQueries = tweedQueries;
+        _userManager = userManager;
     }
 
-    public List<Data.Entities.Tweed> Tweeds { get; } = new();
+    public List<Tweed> Tweeds { get; } = new();
 
     public async Task OnGetAsync()
     {
         var latestTweeds = await _tweedQueries.GetLatestTweeds();
-        Tweeds.AddRange(latestTweeds);
+
+        var tweeds = await Task.WhenAll(latestTweeds.Select(async l => new Tweed
+        {
+            Text = l.Text, CreatedAt = l.CreatedAt,
+            Author = l.AuthorId != null
+                ? (await _userManager.FindByIdAsync(l.AuthorId)).UserName
+                : null
+        }));
+
+        Tweeds.AddRange(tweeds);
+    }
+
+    public class Tweed
+    {
+        public string? Author { get; set; }
+        public string? Text { get; set; }
+        public ZonedDateTime? CreatedAt { get; set; }
     }
 }
