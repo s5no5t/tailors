ce74ddcded933bed06110bcae82306e06f29ea6d
Stephan Oehlert <stephan.oehlert@gmail.com>
2022-12-05 22:36:31 +0100

    Create some fake AppUsers

--- Tweed.Data.GenerateFakes/Program.cs
+++ Tweed.Data.GenerateFakes/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Bogus;
+using Tweed.Data.Entities;
+
+var appUsersFaker = new Faker<AppUser>()
+    .RuleFor(u => u.UserName, (f, _) => f.Internet.UserName());
+
+var appUsers = appUsersFaker.Generate(10);
+
+foreach (var appUser in appUsers)
+    //save
+    Console.WriteLine("AppUser {0} created", appUser.UserName);
