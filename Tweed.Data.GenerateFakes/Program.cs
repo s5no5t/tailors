afc06b78a9e84bbab1002027c2d3c06fc7e185cd
Stephan Oehlert <stephan.oehlert@gmail.com>
2022-12-06 14:04:20 +0100

    Bukl insert test data into ravendb

--- Tweed.Data.GenerateFakes/Program.cs
+++ Tweed.Data.GenerateFakes/Program.cs
@@ -1,7 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Bogus;
+using Microsoft.Extensions.Configuration;
+using Raven.Client.Documents;
+using Raven.DependencyInjection;
 using Tweed.Data.Entities;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
+
+var ravenSettings = config.GetRequiredSection("RavenSettings").Get<RavenSettings>();
+
+using var store = new DocumentStore
+{
+    Urls = ravenSettings.Urls,
+    Database = ravenSettings.DatabaseName
+}.Initialize();
+await using var bulkInsert = store.BulkInsert();
 
 var appUsersFaker = new Faker<AppUser>()
     .RuleFor(u => u.UserName, (f, _) => f.Internet.UserName());
@@ -9,5 +25,7 @@ var appUsersFaker = new Faker<AppUser>()
 var appUsers = appUsersFaker.Generate(10);
 
 foreach (var appUser in appUsers)
-    //save
+{
+    await bulkInsert.StoreAsync(appUser);
     Console.WriteLine("AppUser {0} created", appUser.UserName);
+}
