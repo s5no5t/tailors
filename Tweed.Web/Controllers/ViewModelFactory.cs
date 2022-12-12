d7dd23d7a313dd8877b99b37efffc5ae68595898
Stephan Oehlert <stephan.oehlert@gmail.com>
2022-12-12 13:39:15 +0100

    Use relative date and time for tweed creation

--- Tweed.Web/Controllers/ViewModelFactory.cs
+++ Tweed.Web/Controllers/ViewModelFactory.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using Humanizer;
 using Tweed.Data.Entities;
 using Tweed.Web.Views.Shared;
 
@@ -8,10 +10,14 @@ public static class ViewModelFactory
     internal static TweedViewModel BuildTweedViewModel(Data.Entities.Tweed tweed, long likesCount, AppUser author,
         string currentUserId)
     {
+        var humanizedCreatedAt = tweed.CreatedAt?.LocalDateTime.ToDateTimeUnspecified()
+            .Humanize(true, null, CultureInfo.InvariantCulture);
+
         TweedViewModel viewModel = new()
         {
             Id = tweed.Id,
-            Text = tweed.Text, CreatedAt = tweed.CreatedAt,
+            Text = tweed.Text,
+            CreatedAt = humanizedCreatedAt,
             AuthorId = tweed.AuthorId,
             LikesCount = likesCount,
             LikedByCurrentUser = tweed.Likes.Any(lb => lb.UserId == currentUserId),
--- Tweed.Web/Views/Shared/_Tweed.cshtml.cs
+++ Tweed.Web/Views/Shared/_Tweed.cshtml.cs
@@ -1,5 +1,3 @@
-using NodaTime;
-
 namespace Tweed.Web.Views.Shared;
 
 public class TweedViewModel
@@ -7,7 +5,7 @@ public class TweedViewModel
     public string? AuthorId { get; set; }
     public string? Author { get; set; }
     public string? Text { get; set; }
-    public ZonedDateTime? CreatedAt { get; set; }
+    public string? CreatedAt { get; set; }
     public string? Id { get; set; }
     public long? LikesCount { get; set; }
     public bool LikedByCurrentUser { get; set; }
