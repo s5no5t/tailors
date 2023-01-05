b38b975b73fd3fd7e8ffd98dd84dabf1d9c15c4c
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-01-05 10:57:04 +0100

    Group into modules

--- fsharp/src/Tweed.Web/HttpHandlers.fs
+++ fsharp/src/Tweed.Web/HttpHandlers.fs
@@ -6,35 +6,38 @@ open Giraffe
 open Tweed.Data
 open ViewModels
 
-let indexGetHandler =
-    let tweed = { Content = "Some tweed" }
-    let model = { Tweeds = [ tweed ] }
-    let view = Views.index model
-    htmlView view
+module Index =
+    let indexGetHandler =
+        let tweed = { Content = "Some tweed" }
+        let model = { Tweeds = [ tweed ] }
+        let view = Views.index model
+        htmlView view
 
-[<CLIMutable>]
-type CreateTweed = {
-    Text: string    
-}
+module Tweed =
+    [<CLIMutable>]
+    type CreateTweed = {
+        Text: string    
+    }
 
-let storeTweedHandler = 
-    fun (next : HttpFunc) (ctx : HttpContext) ->
-        task {
-            let! tweed = ctx.BindFormAsync<CreateTweed>()
-            Queries.storeTweed tweed.Text |> ignore
-            return! next ctx
-        }
+    let storeTweedHandler = 
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                let! tweed = ctx.BindFormAsync<CreateTweed>()
+                Queries.storeTweed tweed.Text |> ignore
+                return! next ctx
+            }
 
-let tweedPostHandler =
-    storeTweedHandler
-    >=> redirectTo false "/"
+    let tweedPostHandler =
+        storeTweedHandler
+        >=> redirectTo false "/"
 
-let notFoundHandler =
-    setStatusCode 404 >=> text "Not Found"
+module Fallback =
+    let notFoundHandler =
+        setStatusCode 404 >=> text "Not Found"
 
 let handler : HttpHandler =
     choose [
-        route "/" >=> GET >=> indexGetHandler
-        route "/tweed/create" >=> POST >=> tweedPostHandler 
-        notFoundHandler
+        route "/" >=> GET >=> Index.indexGetHandler
+        route "/tweed/create" >=> POST >=> Tweed.tweedPostHandler 
+        Fallback.notFoundHandler
     ]
