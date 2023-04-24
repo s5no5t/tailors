d455767ab339a2972592a0dec1d47cc549d12442
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-24 14:53:22 +0200

    Remove two factor and personal data forms for now

--- src/Tweed.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/Tweed.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,7 +3,6 @@
 
 #nullable disable
 
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,15 +12,12 @@ namespace Tweed.Web.Areas.Identity.Pages.Account.Manage;
 
 public class IndexModel : PageModel
 {
-    private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
 
     public IndexModel(
-        UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager)
+        UserManager<AppUser> userManager)
     {
         _userManager = userManager;
-        _signInManager = signInManager;
     }
 
     /// <summary>
@@ -39,25 +35,11 @@ public class IndexModel : PageModel
     [TempData]
     public string StatusMessage { get; set; }
 
-    /// <summary>
-    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be
-    ///     used
-    ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
-    [BindProperty]
-    public InputModel Input { get; set; }
-
     private async Task LoadAsync(AppUser user)
     {
         var userName = await _userManager.GetUserNameAsync(user);
-        var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
         Username = userName;
-
-        Input = new InputModel
-        {
-            PhoneNumber = phoneNumber
-        };
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -68,50 +50,5 @@ public class IndexModel : PageModel
 
         await LoadAsync(user);
         return Page();
-    }
-
-    public async Task<IActionResult> OnPostAsync()
-    {
-        var user = await _userManager.GetUserAsync(User);
-        if (user == null)
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-
-        if (!ModelState.IsValid)
-        {
-            await LoadAsync(user);
-            return Page();
-        }
-
-        var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-        if (Input.PhoneNumber != phoneNumber)
-        {
-            var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-            if (!setPhoneResult.Succeeded)
-            {
-                StatusMessage = "Unexpected error when trying to set phone number.";
-                return RedirectToPage();
-            }
-        }
-
-        await _signInManager.RefreshSignInAsync(user);
-        StatusMessage = "Your profile has been updated";
-        return RedirectToPage();
-    }
-
-    /// <summary>
-    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be
-    ///     used
-    ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
-    public class InputModel
-    {
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be
-        ///     used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [Phone]
-        [Display(Name = "Phone number")]
-        public string PhoneNumber { get; set; }
     }
 }
