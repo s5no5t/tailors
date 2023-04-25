f901c06fc5b74cd4fccce2a980ef71813c80f08f
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-26 00:23:16 +0200

    Fix ID schema

---
+++ src/Tweed.Data.Test/AppUserFollowsTest.cs
@@ -0,0 +1,17 @@
+using Tweed.Data.Entities;
+using Xunit;
+
+namespace Tweed.Data.Test;
+
+public class AppUserFollowsTest
+{
+    [Fact]
+    public void BuildId_ShouldReturnCombinedId()
+    {
+        var appUserId = "AppUser/123-A";
+        
+        var followsId = AppUserFollows.BuildId(appUserId);
+        
+        Assert.Equal($"{appUserId}/Follows", followsId);
+    } 
+}
