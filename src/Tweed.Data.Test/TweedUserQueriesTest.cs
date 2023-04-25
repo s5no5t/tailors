987b15ceb8ffdbc0061701344c2820ccdc54ccda
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-25 15:13:11 +0200

    Add document store

--- src/Tweed.Data.Test/TweedUserQueriesTest.cs
+++ src/Tweed.Data.Test/TweedUserQueriesTest.cs
@@ -1,6 +1,15 @@
+using Raven.Client.Documents;
+using Xunit;
+
 namespace Tweed.Data.Test;
 
+[Collection("RavenDb Collection")]
 public class TweedUserQueriesTest
 {
-    
+    private readonly IDocumentStore _store;
+
+    public TweedUserQueriesTest(RavenTestDbFixture ravenDb)
+    {
+        _store = ravenDb.CreateDocumentStore();
+    }
 }
