75520d1154c1ced0de8abddc9fce99c4be344cf3
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-25 13:17:13 +0200

    Update form to allow changing the username

--- src/Tweed.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/Tweed.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,6 +3,7 @@
 
 #nullable disable
 
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,11 +14,14 @@ namespace Tweed.Web.Areas.Identity.Pages.Account.Manage;
 public class IndexModel : PageModel
 {
     private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
 
     public IndexModel(
-        UserManager<AppUser> userManager)
+        UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     /// <summary>
@@ -34,12 +38,20 @@ public class IndexModel : PageModel
     /// </summary>
     [TempData]
     public string StatusMessage { get; set; }
+    
+    [BindProperty]
+    public InputModel Input { get; set; }
 
     private async Task LoadAsync(AppUser user)
     {
         var userName = await _userManager.GetUserNameAsync(user);
 
         Username = userName;
+        
+        Input = new InputModel
+        {
+            Username = userName
+        };
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -50,5 +62,49 @@ public class IndexModel : PageModel
 
         await LoadAsync(user);
         return Page();
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+
+        if (!ModelState.IsValid)
+        {
+            await LoadAsync(user);
+            return Page();
+        }
+
+        var userName = await _userManager.GetUserNameAsync(user);
+        if (Input.Username != userName)
+        {
+            var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.Username);
+            if (!setUserNameResult.Succeeded)
+            {
+                StatusMessage = "Unexpected error when trying to set Username.";
+                return RedirectToPage();
+            }
+        }
+
+        await _signInManager.RefreshSignInAsync(user);
+        StatusMessage = "Your profile has been updated";
+        return RedirectToPage();
+    }
+    
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be
+    ///     used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class InputModel
+    {
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be
+        ///     used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Display(Name = "Username")]
+        public string Username { get; set; }
     }
 }
