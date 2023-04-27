8a2779246f0537c03e5a8b2d4a7657559b2452bb
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-27 11:17:03 +0200

    Move date helper function to extra file

---
+++ src/Tweed.Data.GenerateFakes/DateHelper.cs
@@ -0,0 +1,14 @@
+using NodaTime;
+
+namespace Tweed.Data.GenerateFakes;
+
+internal static class DateHelper
+{
+    internal static ZonedDateTime DateTimeToZonedDateTime(DateTime dateTime)
+    {
+        var localDate = LocalDateTime.FromDateTime(dateTime);
+        var berlinTimeZone = DateTimeZoneProviders.Tzdb["UTC"];
+        var timeZonedDateTime = berlinTimeZone.AtLeniently(localDate);
+        return timeZonedDateTime;
+    }
+}
