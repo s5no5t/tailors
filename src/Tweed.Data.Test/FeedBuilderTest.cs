0d0fcf581ce0eb8d2cf7a3ba5216aba6f1dbde01
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-04-28 12:23:01 +0200

    Implement feed pagination

--- src/Tweed.Data.Test/FeedBuilderTest.cs
+++ src/Tweed.Data.Test/FeedBuilderTest.cs
@@ -9,44 +9,92 @@ using Xunit;
 namespace Tweed.Data.Test;
 
 [Collection("RavenDb Collection")]
-public class FeedBuilderTest
+public class FeedBuilderTest : IAsyncLifetime
 {
     private static readonly ZonedDateTime FixedZonedDateTime =
         new(new LocalDateTime(2022, 11, 18, 15, 20), DateTimeZone.Utc, new Offset());
+
+    private readonly AppUser _currentUser = new();
+
     private readonly IDocumentStore _store;
 
     public FeedBuilderTest(RavenTestDbFixture ravenDb)
     {
         _store = ravenDb.CreateDocumentStore();
     }
-    
+
+    public async Task InitializeAsync()
+    {
+        await using var bulkInsert = _store.BulkInsert();
+
+        for (var i = 0; i < 5; i++)
+        {
+            Model.Tweed currentUserTweed = new()
+            {
+                Text = "test",
+                AuthorId = _currentUser.Id!,
+                CreatedAt = FixedZonedDateTime.PlusHours(-100)
+            };
+            await bulkInsert.StoreAsync(currentUserTweed);
+        }
+
+        List<AppUser> otherUsers = new();
+        for (var i = 0; i < 5; i++)
+        {
+            AppUser otherUser = new();
+            await bulkInsert.StoreAsync(otherUser);
+            otherUsers.Add(otherUser);
+
+            for (var j = 0; j < 20; j++)
+            {
+                Model.Tweed otherUserTweed = new()
+                {
+                    Text = "test",
+                    AuthorId = otherUser.Id!,
+                    CreatedAt = FixedZonedDateTime.PlusHours(-100)
+                };
+                await bulkInsert.StoreAsync(otherUserTweed);
+            }
+        }
+
+        await bulkInsert.StoreAsync(_currentUser);
+
+        AppUserFollows currentUserFollows = new()
+        {
+            AppUserId = _currentUser.Id,
+            Follows = otherUsers.Take(3).Select(u => new Follows
+            {
+                LeaderId = u.Id,
+                CreatedAt = FixedZonedDateTime.PlusHours(-100)
+            }).ToList()
+        };
+        await bulkInsert.StoreAsync(currentUserFollows);
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
     [Fact]
     public async Task GetFeed_ShouldReturnTweedsByCurrentUser()
     {
         using var session = _store.OpenAsyncSession();
         session.Advanced.WaitForIndexesAfterSaveChanges();
-        var currentUser = new AppUser
-        {
-            Id = "currentUser"
-        };
-        await session.StoreAsync(currentUser);
+
         Model.Tweed currentUserTweed = new()
         {
             Text = "test",
-            AuthorId = "currentUser"
+            AuthorId = _currentUser.Id!,
+            CreatedAt = FixedZonedDateTime.PlusHours(1)
         };
         await session.StoreAsync(currentUserTweed);
-        Model.Tweed otherUserTweed = new()
-        {
-            Text = "test",
-            AuthorId = "otherUser"
-        };
-        await session.StoreAsync(otherUserTweed);
+
         await session.SaveChangesAsync();
         AppUserFollowsQueries appUserFollowsQueries = new(session);
         var queries = new FeedBuilder(session, appUserFollowsQueries);
 
-        var tweeds = await queries.GetFeed("currentUser", 0);
+        var tweeds = await queries.GetFeed(_currentUser.Id!, 0);
 
         Assert.Contains(currentUserTweed.Id, tweeds.Select(t => t.Id));
     }
@@ -56,100 +104,70 @@ public class FeedBuilderTest
     {
         using var session = _store.OpenAsyncSession();
         session.Advanced.WaitForIndexesAfterSaveChanges();
-        var currentUser = new AppUser
-        {
-            Id = "currentUser",
-        };
-        await session.StoreAsync(currentUser);
+
+        var followedUser = new AppUser();
+        await session.StoreAsync(followedUser);
+
         AppUserFollows currentUserFollows = new()
         {
-            AppUserId = currentUser.Id,
+            AppUserId = _currentUser.Id,
             Follows = new List<Follows>
             {
                 new()
                 {
-                    LeaderId = "followedUser"
+                    LeaderId = followedUser.Id!
                 }
             }
         };
         await session.StoreAsync(currentUserFollows);
-        var followedUser = new AppUser
-        {
-            Id = "followedUser"
-        };
-        await session.StoreAsync(followedUser);
+
         Model.Tweed followedUserTweed = new()
         {
             Text = "test",
-            AuthorId = "followedUser"
+            AuthorId = followedUser.Id!,
+            CreatedAt = FixedZonedDateTime.PlusHours(1)
         };
         await session.StoreAsync(followedUserTweed);
-        Model.Tweed notFollowedUserTweed = new()
-        {
-            Text = "test",
-            AuthorId = "notFollowedUser"
-        };
-        await session.StoreAsync(notFollowedUserTweed);
+
         await session.SaveChangesAsync();
         AppUserFollowsQueries appUserFollowsQueries = new(session);
         var queries = new FeedBuilder(session, appUserFollowsQueries);
 
-        var tweeds = await queries.GetFeed("currentUser", 0);
+        var tweeds = await queries.GetFeed(_currentUser.Id!, 0);
 
         Assert.Contains(tweeds, t => t.Id == followedUserTweed.Id);
     }
 
     [Fact]
-    public async Task GetFeed_ShouldReturnOrderedTweeds()
+    public async Task GetFeed_ShouldNotReturnTheSameTweedTwice()
     {
+        _store.OnBeforeQuery += (sender, beforeQueryExecutedArgs) =>
+        {
+            beforeQueryExecutedArgs.QueryCustomization.WaitForNonStaleResults();
+        };
         using var session = _store.OpenAsyncSession();
-        session.Advanced.WaitForIndexesAfterSaveChanges();
-        var currentUser = new AppUser
-        {
-            Id = "currentUser"
-        };
-        await session.StoreAsync(currentUser);
-        Model.Tweed olderTweed = new()
-        {
-            Text = "older tweed",
-            AuthorId = "currentUser",
-            CreatedAt = FixedZonedDateTime
-        };
-        await session.StoreAsync(olderTweed);
-        var recent = FixedZonedDateTime.PlusHours(1);
-        Model.Tweed recentTweed = new()
-        {
-            Text = "recent tweed",
-            AuthorId = "currentUser",
-            CreatedAt = recent
-        };
-        await session.StoreAsync(recentTweed);
-        await session.SaveChangesAsync();
+
         AppUserFollowsQueries appUserFollowsQueries = new(session);
         var queries = new FeedBuilder(session, appUserFollowsQueries);
 
-        var tweeds = (await queries.GetFeed("currentUser", 0)).ToList();
+        var tweeds = (await queries.GetFeed(_currentUser.Id!, 0)).ToList();
 
-        Assert.Equal(recentTweed.Id, tweeds[0].Id);
-        Assert.Equal(olderTweed.Id, tweeds[1].Id);
+        var anyDuplicateTweed = tweeds.GroupBy(x => x.Id).Any(g => g.Count() > 1);
+        Assert.False(anyDuplicateTweed);
     }
 
     [Fact]
-    public async Task GetFeed_ShouldReturn20Tweeds()
+    public async Task GetFeed_ShouldReturnPageSizeTweeds()
     {
         using var session = _store.OpenAsyncSession();
         session.Advanced.WaitForIndexesAfterSaveChanges();
-        var currentUser = new AppUser
-        {
-            Id = "currentUser"
-        };
-        await session.StoreAsync(currentUser);
+
         for (var i = 0; i < 25; i++)
         {
             Model.Tweed tweed = new()
             {
                 Text = "test",
-                AuthorId = "currentUser",
+                AuthorId = _currentUser.Id!,
                 CreatedAt = FixedZonedDateTime
             };
             await session.StoreAsync(tweed);
@@ -159,8 +177,23 @@ public class FeedBuilderTest
         AppUserFollowsQueries appUserFollowsQueries = new(session);
         var queries = new FeedBuilder(session, appUserFollowsQueries);
 
-        var tweeds = (await queries.GetFeed("currentUser", 0)).ToList();
+        var feed = (await queries.GetFeed(_currentUser.Id!, 0)).ToList();
 
-        Assert.Equal(20, tweeds.Count);
+        Assert.Equal(FeedBuilder.PageSize, feed.Count);
+    }
+
+    [Fact]
+    public async Task GetFeed_ShouldRespectPage()
+    {
+        using var session = _store.OpenAsyncSession();
+        session.Advanced.WaitForIndexesAfterSaveChanges();
+
+        AppUserFollowsQueries appUserFollowsQueries = new(session);
+        var queries = new FeedBuilder(session, appUserFollowsQueries);
+
+        var page0Feed = (await queries.GetFeed(_currentUser.Id!, 0)).ToList();
+        var page1Feed = (await queries.GetFeed(_currentUser.Id!, 1)).ToList();
+
+        Assert.NotEqual(page0Feed[0].Id, page1Feed[1].Id);
     }
 }
