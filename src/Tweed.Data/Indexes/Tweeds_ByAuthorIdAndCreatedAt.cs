bb0de78a948799eb5dc6bb54ed7f74e32d7d1f3f
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-03 13:03:12 +0200

    Split indexes into extra files

--- src/Tweed.Data/Indexes/Tweeds_ByAuthorIdAndCreatedAt.cs
+++ src/Tweed.Data/Indexes/Tweeds_ByAuthorIdAndCreatedAt.cs
@@ -14,18 +14,3 @@ public class Tweeds_ByAuthorIdAndCreatedAt : AbstractIndexCreationTask<Model.Twe
             };
     }
 }
-
-public class Tweeds_ByText : AbstractIndexCreationTask<Model.Tweed>
-{
-    public Tweeds_ByText()
-    {
-        Map = tweeds => from tweed in tweeds
-            select new
-            {
-                tweed.Id,
-                tweed.Text
-            };
-
-        Analyzers.Add(a => a.Text, "StandardAnalyzer");
-    }
-}
