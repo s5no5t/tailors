ab33fdf82d2341758a188618a79c33b64b26b63d
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-03 20:37:52 +0200

    Create thread if it doesn't exist yet

--- src/Tweed.Data/ThreadQueries.cs
+++ src/Tweed.Data/ThreadQueries.cs
@@ -13,6 +13,11 @@ public class ThreadQueries
 
     public async Task AddTweedToThread(string tweedId, string parentTweedId, string threadId)
     {
-        throw new NotImplementedException();
+        var thread = await _session.LoadAsync<Model.Thread>(threadId);
+        if (thread is null)
+        {
+            thread = new Model.Thread();
+            await _session.StoreAsync(thread);
+        }
     }
 }
