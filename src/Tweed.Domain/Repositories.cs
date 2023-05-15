b90cecc112b6ec818f8dcb0afddc655f14a4d0db
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-15 11:34:35 +0200

    Simplify method params

--- src/Tweed.Domain/Repositories.cs
+++ src/Tweed.Domain/Repositories.cs
@@ -29,10 +29,7 @@ public interface ITweedRepository
     Task<List<Model.Tweed>> GetByAuthorId(string authorId, int feedSize = 20);
     Task Create(Model.Tweed tweed);
     Task<List<Model.Tweed>> Search(string term);
-
-    Task<List<Model.Tweed>> GetExtraTweeds(List<Model.Tweed> ownTweeds,
-        List<Model.Tweed> followerTweeds, int count);
-
+    Task<List<Model.Tweed>> GetRecentTweeds(List<string> ignoreTweedIds, int count);
     Task<List<Model.Tweed>> GetFollowerTweeds(List<string?> followedUserIds, int count);
 }
 
