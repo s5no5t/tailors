1524bc5192c03855259a7bd9a891c4414bbe5418
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-16 23:33:42 +0200

    Use Result instead of Exception for Tweed use case errors

--- src/Tweed.Web/Views/Tweed/_CreateReplyTweed.cshtml.cs
+++ src/Tweed.Web/Views/Tweed/_CreateReplyTweed.cshtml.cs
@@ -10,5 +10,5 @@ public class CreateReplyTweedViewModel
     [StringLength(280)]
     public string Text { get; set; } = "";
 
-    public string? ParentTweedId { get; set; }
+    public string ParentTweedId { get; set; } = "";
 }
