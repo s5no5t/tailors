2b4271764aa49388902d64e033e72aa5ba62f548
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-16 22:55:59 +0200

    Inject dependencies in controller method, not constructor

--- src/Tweed.Web/Controllers/SearchController.cs
+++ src/Tweed.Web/Controllers/SearchController.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Tweed.Thread.Domain;
-using Tweed.User;
 using Tweed.User.Domain;
 using Tweed.Web.Views.Search;
 
@@ -11,15 +10,6 @@ namespace Tweed.Web.Controllers;
 [Authorize]
 public class SearchController : Controller
 {
-    private readonly IUserRepository _userRepository;
-    private readonly ITweedRepository _tweedRepository;
-
-    public SearchController(ITweedRepository tweedRepository, IUserRepository userRepository)
-    {
-        _tweedRepository = tweedRepository;
-        _userRepository = userRepository;
-    }
-
     public IActionResult Index()
     {
         IndexViewModel viewModel = new("", new List<UserViewModel>(), new List<TweedViewModel>());
@@ -28,14 +18,16 @@ public class SearchController : Controller
 
     public async Task<IActionResult> Results(
         [FromQuery] [Required] [StringLength(50, MinimumLength = 3)] [RegularExpression(@"^[\w\s]*$")]
-        string term)
+        string term,
+        [FromServices] ITweedRepository tweedRepository,
+        [FromServices] IUserRepository userRepository)
     {
         if (!ModelState.IsValid)
             return View("Index",
                 new IndexViewModel(term, new List<UserViewModel>(), new List<TweedViewModel>()));
 
-        var users = await _userRepository.Search(term);
-        var tweeds = await _tweedRepository.Search(term);
+        var users = await userRepository.Search(term);
+        var tweeds = await tweedRepository.Search(term);
         IndexViewModel viewModel = new(
             term,
             users.Select(u => new UserViewModel(u.Id!, u.UserName!)).ToList(),
