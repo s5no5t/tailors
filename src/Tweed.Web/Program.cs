280ace78bb7bbae47c2cfc6f9e986512f320bd71
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-05-22 09:45:32 +0200

    Refactor into separate methods

--- src/Tweed.Web/Program.cs
+++ src/Tweed.Web/Program.cs
@@ -16,79 +16,15 @@ using IdentityRole = Raven.Identity.IdentityRole;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages()
-    .AddMvcOptions(options => options.Filters.Add<RavenSaveChangesAsyncPageFilter>());
-
+builder.Services.AddRazorPages().AddMvcOptions(options => options.Filters.Add<RavenSaveChangesAsyncPageFilter>());
 builder.Services.AddControllersWithViews(o => o.Filters.Add<RavenSaveChangesAsyncActionFilter>());
-
-builder.Services.AddRavenDbDocStore(options =>
-{
-    options.BeforeInitializeDocStore = store =>
-    {
-        store.PreInitialize();
-        store.PreInitializeLikes();
-    };
-    options.AfterInitializeDocStore = store =>
-    {
-        store.EnsureDatabaseExists();
-        store.DeployUserIndexes();
-        store.DeployTweedIndexes();
-    };
-});
-builder.Services.AddRavenDbAsyncSession();
-
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-
-builder.Services
-    .AddIdentity<AppUser, IdentityRole>(options =>
-    {
-        options.User.AllowedUserNameCharacters =
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
-    })
-    .AddRavenDbIdentityStores<AppUser,
-        IdentityRole>(
-        _ => // empty options is a workaround for an exception in case this param is null
-        {
-        })
-    .AddDefaultTokenProviders();
-
-builder.Services.ConfigureApplicationCookie(
-    options => options.LoginPath = "/Identity/Account/login");
-
-builder.Services.Scan(scan =>
-{
-    scan.FromCallingAssembly().AddClasses().AsMatchingInterface();
-    scan.FromAssembliesOf(typeof(UserRepository)).AddClasses().AsMatchingInterface();
-    scan.FromAssembliesOf(typeof(AppUser)).AddClasses().AsMatchingInterface();
-    scan.FromAssembliesOf(typeof(ShowFeedUseCase)).AddClasses().AsMatchingInterface();
-    scan.FromAssembliesOf(typeof(UserLikes)).AddClasses().AsMatchingInterface();
-    scan.FromAssembliesOf(typeof(Tweed.Thread.Domain.Tweed)).AddClasses().AsMatchingInterface();
-});
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-});
 
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddRazorPages();
-
-var honeycombOptions = builder.Configuration.GetHoneycombOptions();
-
-// Setup OpenTelemetry Tracing
-builder.Services.AddOpenTelemetry().WithTracing(otelBuilder =>
-{
-    otelBuilder
-        .AddHoneycomb(honeycombOptions)
-        .AddAspNetCoreInstrumentationWithBaggage()
-        .AddCommonInstrumentations();
-});
+SetupRavenDbServices(builder);
+SetupIdentity(builder);
+SetupOpenTelemetry(builder);
+SetupAssemblyScanning(builder);
 
 builder.Services.AddHostedService<TweedThreadUpdateSubscriptionWorker>();
 
@@ -111,3 +47,79 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+static void SetupRavenDbServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddRavenDbDocStore(options =>
+    {
+        options.BeforeInitializeDocStore = store =>
+        {
+            store.PreInitialize();
+            store.PreInitializeLikes();
+        };
+        options.AfterInitializeDocStore = store =>
+        {
+            store.EnsureDatabaseExists();
+            store.DeployUserIndexes();
+            store.DeployTweedIndexes();
+        };
+    });
+    builder.Services.AddRavenDbAsyncSession();
+}
+
+static void SetupIdentity(WebApplicationBuilder builder)
+{
+    builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+    builder.Services
+        .AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+        })
+        .AddRavenDbIdentityStores<AppUser,
+            IdentityRole>(
+            _ => // empty options is a workaround for an exception in case this param is null
+            {
+            })
+        .AddDefaultTokenProviders();
+
+    builder.Services.ConfigureApplicationCookie(
+        options => options.LoginPath = "/Identity/Account/login");
+
+    
+    builder.Services.Configure<IdentityOptions>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+    });
+}
+
+static void SetupOpenTelemetry(WebApplicationBuilder builder)
+{
+    var honeycombOptions = builder.Configuration.GetHoneycombOptions();
+    builder.Services.AddOpenTelemetry().WithTracing(otelBuilder =>
+    {
+        otelBuilder
+            .AddHoneycomb(honeycombOptions)
+            .AddAspNetCoreInstrumentationWithBaggage()
+            .AddCommonInstrumentations();
+    });
+}
+
+static void SetupAssemblyScanning(WebApplicationBuilder builder)
+{
+    builder.Services.Scan(scan =>
+    {
+        scan.FromCallingAssembly().AddClasses().AsMatchingInterface();
+        scan.FromAssembliesOf(typeof(UserRepository)).AddClasses().AsMatchingInterface();
+        scan.FromAssembliesOf(typeof(AppUser)).AddClasses().AsMatchingInterface();
+        scan.FromAssembliesOf(typeof(ShowFeedUseCase)).AddClasses().AsMatchingInterface();
+        scan.FromAssembliesOf(typeof(UserLikes)).AddClasses().AsMatchingInterface();
+        scan.FromAssembliesOf(typeof(Tweed.Thread.Domain.Tweed)).AddClasses().AsMatchingInterface();
+    });
+}
