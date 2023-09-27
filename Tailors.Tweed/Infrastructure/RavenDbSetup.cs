d23e0dbfbfaa7f76e470b51dba438050cda96e63
Stephan Oehlert <soehlert@ecademy-learning.com>
2023-09-27 13:56:20 +0200

    Fix some warnings

--- Tailors.Tweed/Infrastructure/RavenDbSetup.cs
+++ Tailors.Tweed/Infrastructure/RavenDbSetup.cs
@@ -5,15 +5,6 @@ namespace Tailors.Tweed.Infrastructure;
 
 public static class RavenDbSetup
 {
-    public static void PreInitializeTweeds(this IDocumentStore store)
-    {
-        store.ApplyCustomConventions();
-    }
-
-    private static void ApplyCustomConventions(this IDocumentStore store)
-    {
-    }
-
     public static void DeployTweedIndexes(this IDocumentStore store)
     {
         new TweedsByAuthorIdAndCreatedAt().Execute(store);
