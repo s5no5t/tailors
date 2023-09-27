f3bcdac64299e54a6202327f77873e090a941219
Stephan Oehlert <soehlert@ecademy-learning.com>
2023-09-27 14:24:41 +0200

    Revert "Fix some warnings"
    This reverts commit 695aacbcac76b93ad56307e0923a7aef3a5b4bed.

--- src/Tailors.Thread.Test/Domain/MoqExtensions.cs
+++ src/Tailors.Thread.Test/Domain/MoqExtensions.cs
@@ -2,17 +2,12 @@ using Moq;
 
 namespace Tailors.Thread.Test.Domain;
 
-public static class MoqExtensions
+public class MoqExtensions
 {
-    public static IEnumerable<T> CollectionMatcher<T>(IEnumerable<T> expectations)
+    public static IEnumerable<T> CollectionMatcher<T>(IEnumerable<T> expectation)
     {
-        var expectationsList = expectations.ToList();
-
         return Match.Create((IEnumerable<T> inputCollection) =>
-        {
-            var input = inputCollection.ToList();
-            return expectationsList.Except(input).Any() &&
-                   !input.Except(expectationsList).Any();
-        });
+            !expectation.Except(inputCollection).Any() &&
+            !inputCollection.Except(expectation).Any());
     }
 }
