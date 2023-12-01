7ebb987e5a30d764699eedfa1c2a58a599d5152c
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-12-01 09:47:28 +0100

    Init some basic ravendb config

--- src/Tailors.BlazorWeb/Program.cs
+++ src/Tailors.BlazorWeb/Program.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
+using Raven.DependencyInjection;
 using Raven.Identity;
 using Tailors.BlazorWeb.Components;
 using Tailors.BlazorWeb.Components.Account;
+using Tailors.Domain.ThreadAggregate;
+using Tailors.Domain.TweedAggregate;
 using Tailors.Domain.UserAggregate;
+using Tailors.Domain.UserFollowsAggregate;
+using Tailors.Domain.UserLikesAggregate;
+using Tailors.Infrastructure;
+using Tailors.Infrastructure.ThreadAggregate;
+using Tailors.Infrastructure.TweedAggregate;
+using Tailors.Infrastructure.UserAggregate;
+using Tailors.Infrastructure.UserFollowsAggregate;
+using Tailors.Infrastructure.UserLikesAggregate;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,12 +35,15 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-builder.Services.AddIdentityCore<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRavenDbIdentityStores<AppUser>()
+builder.Services.AddIdentityCore<AppUser>(options =>
+        options.SignIn.RequireConfirmedAccount = true)
     .AddSignInManager()
-    .AddDefaultTokenProviders();
+    .AddRavenDbIdentityStores<AppUser>();
 
 builder.Services.AddSingleton<IEmailSender<AppUser>, IdentityNoOpEmailSender>();
+
+SetupRavenDbServices(builder);
+SetupAssemblyScanning(builder);
 
 var app = builder.Build();
 
@@ -55,3 +69,33 @@ app.MapRazorComponents<App>();
 app.MapAdditionalIdentityEndpoints();
 
 app.Run();
+
+static void SetupRavenDbServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddRavenDbDocStore(options =>
+    {
+        options.BeforeInitializeDocStore = store => { store.PreInitialize(); };
+        options.AfterInitializeDocStore = store =>
+        {
+            store.EnsureDatabaseExists();
+            store.DeployIndexes();
+        };
+    });
+    builder.Services.AddRavenDbAsyncSession();
+}
+
+static void SetupAssemblyScanning(WebApplicationBuilder builder)
+{
+    builder.Services.AddScoped<IThreadRepository, ThreadRepository>();
+    builder.Services.AddScoped<ITweedRepository, TweedRepository>();
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IUserFollowsRepository, UserFollowsRepository>();
+    builder.Services.AddScoped<IUserLikesRepository, UserLikesRepository>();
+    //builder.Services.AddScoped<INotificationManager, NotificationManager>();
+    builder.Services.AddScoped<FollowUserUseCase>();
+    builder.Services.AddScoped<ShowFeedUseCase>();
+    builder.Services.AddScoped<ThreadUseCase>();
+    builder.Services.AddScoped<CreateTweedUseCase>();
+    builder.Services.AddScoped<LikeTweedUseCase>();
+    //builder.Services.AddScoped<TweedViewModelFactory>();
+}
