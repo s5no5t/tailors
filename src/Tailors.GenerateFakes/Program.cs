e504f103adbff52dbd2f93d3278b3f1129447407
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-12-04 15:21:32 +0100

    Run all fake tweeds through thread gen

--- src/Tailors.GenerateFakes/Program.cs
+++ src/Tailors.GenerateFakes/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Raven.DependencyInjection;
+using Tailors.Domain.ThreadAggregate;
 using Tailors.GenerateFakes;
+using Tailors.Infrastructure.ThreadAggregate;
+using Tailors.Infrastructure.TweedAggregate;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -11,9 +14,19 @@ using var store = DocumentStoreHelper.OpenDocumentStore(ravenSettings);
 
 var dataFakerSettings =
     config.GetRequiredSection("DataFakerSettings").Get<DataFakerSettings>();
+
 var dataFaker = new DataFaker(store, dataFakerSettings);
 
 var users = await dataFaker.CreateFakeUsers();
 await dataFaker.CreateFakeFollows(users);
 var tweeds = await dataFaker.CreateFakeTweeds(users);
+
+foreach (var tweed in tweeds)
+{
+    using var session = store.OpenAsyncSession();
+    var threadUseCase = new ThreadUseCase(new ThreadRepository(session), new TweedRepository(session));
+    await threadUseCase.AddTweedToThread(tweed.Id!);
+    await session.SaveChangesAsync();
+}
+
 await dataFaker.CreateFakeLikes(users, tweeds);
