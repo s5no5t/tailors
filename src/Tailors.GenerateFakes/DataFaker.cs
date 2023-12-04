23b9b0320b932c22e1bfc73b61ab28f880b079a9
Stephan Oehlert <stephan.oehlert@gmail.com>
2023-12-04 22:49:10 +0100

    Fix warning

--- src/Tailors.GenerateFakes/DataFaker.cs
+++ src/Tailors.GenerateFakes/DataFaker.cs
@@ -66,8 +66,7 @@ internal class DataFaker
                 _faker.Lorem.Paragraph(1),
                 _faker.Date.Past(),
                 null,
-                tweeds.Count > 0 && _faker.Random.Bool() ? tweeds[i - 1].Id : null,
-                null);
+                tweeds.Count > 0 && _faker.Random.Bool() ? tweeds[i - 1].Id : null);
             await bulkInsert.StoreAsync(tweed);
             tweeds.Add(tweed);
         }
